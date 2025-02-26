2d74445339ee8db27ce6ca27c2d33728a79a8b1d
Arsenii <arsenii.nedorezov@gmail.com>
2025-02-26 10:18:26 +0100

    add an orbit control

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import './style.css'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set( 0, 1, 0 );
+controls.update();
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -16,8 +22,10 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
+    requestAnimationFrame( animate );
+
+    controls.update();
+
     renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate );
