c478f9e1a2da832c5c5fa13372366a3d7b7d71b2
Arsenii Nedorezov <arsenii.nedorezov@gmail.com>
2025-03-26 08:46:18 +0100

    fix Orbit Controls import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import GUI from 'lil-gui';
 import './style.css'
 
